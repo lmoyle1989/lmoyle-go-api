5697986e6cdb1c376eb0b1e1388ed6951cf4fb33
Lucas Moyle <lmoyle@gmail.com>
2024-10-10 15:17:55 -0700

    add basic root path handler

--- my_api.go
+++ my_api.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"database/sql"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3" 
 )
 
 type Page struct {
@@ -24,9 +29,28 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func root_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Slug: %s", r.URL.Path[1:])
+}
+
 func main() {
+	os.Remove("TestPage.txt")
+	os.Remove("sqlite-database.db")
+
+	file, err := os.Create("sqlite-database.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	
+	mydb, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer mydb.Close()
+
 	p1 := &Page{Title: "TestPage", Body: []byte("This is sample page content.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/", root_handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
