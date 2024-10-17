db752762e3ff47b9c6f80700c4fb1245df276fd9
Lucas Moyle <lmoyle@gmail.com>
2024-10-17 15:11:38 -0700

    add basic sql stuff

--- my_api.go
+++ my_api.go
@@ -7,32 +7,31 @@ import (
 	"database/sql"
 	// "html/template"
 	"net/http"
-
+	"time"
 	_ "github.com/mattn/go-sqlite3" 
 )
 
 type Page struct {
+	Id int
 	Title string
 	Body []byte
+	CreatedAt string
+	UpdatedAt string
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
+// func (p *Page) save() error {
+// 	filename := p.Title + ".txt"
+// 	return os.WriteFile(filename, p.Body, 0600)
+// }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func root_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Slug: %s\nHeader: %s", r.URL.Path[1:], r.Header)
-}
+// func loadPage(title string) (*Page, error) {
+// 	filename := title + ".txt"
+// 	body, err := os.ReadFile(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &Page{Title: title, Body: body}, nil
+// }
 
 func main() {
 	// os.Remove("TestPage.txt")
@@ -47,6 +46,13 @@ func main() {
 	mydb, _ := sql.Open("sqlite3", "./sqlite-database.db")
 	defer mydb.Close()
 
+	initializeDB(mydb)
+	
+	insertPage(mydb, "Test Title", []byte("Here is some sample text for my initial page."))
+	insertPage(mydb, "Title #2", []byte("Here is some sample text for my second page."))
+
+	getPages(mydb)
+
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is sample page content.")}
 	// p1.save()
 	// p2, _ := loadPage("TestPage")
@@ -56,4 +62,52 @@ func main() {
 	mux.HandleFunc("GET /", root_handler)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func root_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Slug: %s\nHeader: %s", r.URL.Path[1:], r.Header)
+}
+
+func initializeDB(db *sql.DB) {
+	initPageSQL := `CREATE TABLE IF NOT EXISTS page (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		body BLOB,
+		created_at TEXT,
+		updated_at TEXT
+	);`
+	statement, err := db.Prepare(initPageSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec()
+}
+
+func insertPage(db *sql.DB, title string, body []byte) {
+	insertPageSQL := `INSERT INTO page(title, body, created_at, updated_at) VALUES(?, ?, ?, ?)`
+	statement, err := db.Prepare(insertPageSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	created_at := time.Now().Format(time.RFC3339)
+	updated_at := created_at
+	statement.Exec(title, body, created_at, updated_at)
+}
+
+func getPages(db *sql.DB) {
+	rows, err := db.Query(`SELECT * FROM page`)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+	var pages []Page
+	for rows.Next() {
+		var page Page
+		rows.Scan(&page.Id, &page.Title, &page.Body, &page.CreatedAt, &page.UpdatedAt)
+		pages = append(pages, page)
+	}
+	
+	for _, myrow := range pages {
+		fmt.Println(string(myrow.Body))
+	}
 }
