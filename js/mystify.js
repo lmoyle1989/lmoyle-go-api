2c76fae9f095b5af710bb6032a7ec187d08fdaec
Lucas Moyle <lmoyle@gmail.com>
2025-01-10 10:54:34 -0800

    first animation

--- js/mystify.js
+++ js/mystify.js
@@ -1,1 +1,31 @@
-alert("JS is working on reload!!!");
+const startTime = new Date();
+const ctx = document.querySelector("#mainCanvas").getContext("2d");
+height = 640
+width = 800
+
+window.onload = function() {
+  mystifyInit();
+}
+
+function mystifyInit() {
+  window.requestAnimationFrame(mystifyDraw);
+}
+
+function mystifyDraw() {
+  ctx.globalCompositeOperation = "destination-over";
+  ctx.clearRect(0, 0, width, height);
+
+  const currentTime = new Date();
+  const timeDiff = currentTime - startTime;
+
+  dotxpos = 800 * ((timeDiff % 10000) / 10000);
+  const mydot = new Path2D();
+  mydot.arc(dotxpos, 320, 15, 0, 2 * Math.PI);
+  ctx.fill(mydot);
+
+  window.requestAnimationFrame(mystifyDraw);
+}
+
+function timeSinceLoad() {
+  return 
+}
