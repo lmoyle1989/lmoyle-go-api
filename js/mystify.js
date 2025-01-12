fea6762a491795c704a6541720ff54ad0b7b6f1d
Lucas Moyle <lmoyle@gmail.com>
2025-01-12 10:29:41 -0800

    now its a moving square

--- js/mystify.js
+++ js/mystify.js
@@ -4,6 +4,7 @@ const height = canvas.height;
 const width = canvas.width;
 const startTime = new Date();
 let prevTime = 0;
+let baseSpeed = 1;
 
 function Point(px, py, dx, dy) {
   this.px = px;
@@ -13,8 +14,8 @@ function Point(px, py, dx, dy) {
 }
 
 Point.prototype.updatePos = function(step) {
-  let newx = this.px + ((step * this.dx) / 10);
-  let newy = this.py + ((step * this.dy) / 10);
+  let newx = this.px + ((this.dx * step * baseSpeed) / 10);
+  let newy = this.py + ((this.dy * step * baseSpeed) / 10);
 
   if (newx < 0 || newx > width) {
     this.dx *= -1;
@@ -34,8 +35,8 @@ Point.prototype.updatePos = function(step) {
 
 const my1Point = new Point(400, 320, 1, 1);
 const my2Point = new Point(400, 320, 1, -1);
-const my3Point = new Point(400, 320, -1, 1);
-const my4Point = new Point(400, 320, -1, -1);
+const my3Point = new Point(400, 320, -1, -1);
+const my4Point = new Point(400, 320, -1, 1);
 
 window.onload = function() {
   mystifyInit();
@@ -46,25 +47,34 @@ function mystifyInit() {
 }
 
 function myDraw(timestamp) {
+  // what if i just keep a deque of previous point locations...?
   const step = timestamp - prevTime;
 
   ctx.clearRect(0, 0, width, height);
   
-  ctx.beginPath();
-  ctx.arc(my1Point.px, my1Point.py, 15, 0, 2 * Math.PI);
-  ctx.fill();
+  // ctx.beginPath();
+  // ctx.arc(my1Point.px, my1Point.py, 15, 0, 2 * Math.PI);
+  // ctx.fill();
+
+  // ctx.beginPath();
+  // ctx.arc(my2Point.px, my2Point.py, 15, 0, 2 * Math.PI);
+  // ctx.fill();
+
+  // ctx.beginPath();
+  // ctx.arc(my3Point.px, my3Point.py, 15, 0, 2 * Math.PI);
+  // ctx.fill();
+
+  // ctx.beginPath();
+  // ctx.arc(my4Point.px, my4Point.py, 15, 0, 2 * Math.PI);
+  // ctx.fill();
 
   ctx.beginPath();
-  ctx.arc(my2Point.px, my2Point.py, 15, 0, 2 * Math.PI);
-  ctx.fill();
-
-  ctx.beginPath();
-  ctx.arc(my3Point.px, my3Point.py, 15, 0, 2 * Math.PI);
-  ctx.fill();
-
-  ctx.beginPath();
-  ctx.arc(my4Point.px, my4Point.py, 15, 0, 2 * Math.PI);
-  ctx.fill();
+  ctx.moveTo(my1Point.px, my1Point.py);
+  ctx.lineTo(my2Point.px, my2Point.py);
+  ctx.lineTo(my3Point.px, my3Point.py);
+  ctx.lineTo(my4Point.px, my4Point.py);
+  ctx.closePath();
+  ctx.stroke();
 
   my1Point.updatePos(step);
   my2Point.updatePos(step);
