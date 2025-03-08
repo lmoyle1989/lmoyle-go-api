a07dacc94a7cff938822b7fd62b441e66d3348fe
Lucas Moyle <lmoyle@gmail.com>
2025-03-08 12:52:47 -0800

    add really basic 3d tube render

--- js/pipes.js
+++ js/pipes.js
@@ -1,26 +1,41 @@
 import * as THREE from 'three';
 
 const viewport = document.querySelector("#mainCanvas");
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, viewport.clientWidth / viewport.clientHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( viewport.clientWidth, viewport.clientHeight );
 viewport.appendChild( renderer.domElement );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, viewport.clientWidth / viewport.clientHeight, 1, 500 );
+camera.lookAt(0,0,0);
+camera.position.set(0,0,50);
 
+scene.add( new THREE.AmbientLight( 0x777777 ) );
+const light = new THREE.DirectionalLight( 0xffffff, 3 );
+light.position.set( 100, 100, 100 );
+scene.add( light );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const curve = new THREE.CatmullRomCurve3( [
+	new THREE.Vector3( -10, 0, 0 ),
+	new THREE.Vector3( -5, 5, 0 ),
+	new THREE.Vector3( 0, 0, 0 ),
+	new THREE.Vector3( 5, -5, 0 ),
+	new THREE.Vector3( 10, 0, 0 )
+] );
 
-camera.position.z = 5;
+const materialargs = {
+	color: 0xffffff,
+	specular: 0x050505,
+	shininess: 500,
+	emissive: 0x000000
+};
+const material = new THREE.MeshPhongMaterial( materialargs );
+const geometry = new THREE.TubeGeometry(curve);
+const mesh = new THREE.Mesh( geometry, material );
+scene.add(mesh);
 
 
 function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 
