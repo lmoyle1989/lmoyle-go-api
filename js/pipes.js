5c5bbef06cd0b2d7fbd2b024a06e573c2a47085e
Lucas Moyle <lmoyle@gmail.com>
2025-03-11 16:58:26 -0700

    code improvements

--- js/pipes.js
+++ js/pipes.js
@@ -1,20 +1,5 @@
 import * as THREE from 'three';
 
-
-
-
-
-// const xmax = 100;
-// const xmin = -100;
-// const ymax = 100;
-// const ymin = -100;
-// const zmax = 100;
-// const zmin = -100;
-
-// function validCoords(coords) {
-// 	return coords.x <= xmax && coords.x >= xmin && coords.y <= ymax && coords.y >= ymin && coords.z <= zmax && coords.z >= zmin
-// }
-
 const dirs = [
 	{x: 1, y: 0, z: 0},
 	{x: -1, y: 0, z: 0},
@@ -32,8 +17,8 @@ function shuffleArray(array) {
   return array;
 }
 
-function getRandomNonZeroInt(max) {
-  return Math.floor(Math.random() * max) + 1;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 class PipeRun {
@@ -47,30 +32,28 @@ class PipeRun {
 
 	generatePath() {
 		let cur = this.startpos;
-		while (this.maxlength > 0) {
+		let i;
+		let nextdir;
+		let nextpos;
+		const l = 5;
+		while (this.maxlength >= 0) {
 			this.visited.add(JSON.stringify(cur));
 			this.path.push(cur);
-			shuffleArray(dirs);
-			let l = 3; // getRandomNonZeroInt(4); We can do this... but we need to ensure that a pipe cant go back on itself
-			let i = 0;
-			let nextdir = dirs[i];
-			let nextpos = {x: cur.x + (nextdir.x * l), y: cur.y + (nextdir.y * l), z: cur.z + (nextdir.z * l)};
-			// while ( !validCoords(nextpos) || this.visited.has(JSON.stringify(nextpos)) ) {
-			while ( this.visited.has(JSON.stringify(nextpos)) ) {
-				i += 1;
+			do {
+				i = getRandomInt(dirs.length)
 				nextdir = dirs[i];
 				nextpos = {x: cur.x + (nextdir.x * l), y: cur.y + (nextdir.y * l), z: cur.z + (nextdir.z * l)}; // dont repeat this
+			} while ( this.visited.has(JSON.stringify(nextpos)) );
+			if (this.maxlength != 0) {
+				for (let j = 1; j < l; j++) {
+					this.path.push({x: cur.x + (nextdir.x * j), y: cur.y + (nextdir.y * j), z: cur.z + (nextdir.z * j)});
+				}
 			}
 			cur = nextpos;
 			this.maxlength -= 1;
 		}
 	}
 }
-
-
-
-
-
 
 const renderer = new THREE.WebGLRenderer();
 const viewport = document.querySelector("#mainCanvas");
@@ -88,49 +71,6 @@ light.position.set( 100, 100, 100 );
 scene.add( light );
 scene.add( new THREE.AmbientLight( 0x777777 ) );
 
-const testpipes = [
-	new THREE.LineCurve3(
-		new THREE.Vector3( 10, 7.5, 0 ),
-		new THREE.Vector3( -7.5, 7.5, 0 )
-	),
-	new THREE.QuadraticBezierCurve3(
-		new THREE.Vector3( -7.5, 7.5, 0 ),
-		new THREE.Vector3( -10, 7.5, 0 ),
-		new THREE.Vector3( -10, 5, 0 ),
-	),
-	new THREE.LineCurve3(
-		new THREE.Vector3( -10, 5, 0 ),
-		new THREE.Vector3( -10, -5, 0 )
-	),
-	new THREE.QuadraticBezierCurve3(
-		new THREE.Vector3( -10, -5, 0 ),
-		new THREE.Vector3( -10, -7.5, 0 ),
-		new THREE.Vector3( -7.5, -7.5, 0 )
-	),
-	new THREE.LineCurve3(
-		new THREE.Vector3( -7.5, -7.5, 0 ),
-		new THREE.Vector3( 0, -7.5, 0 )
-	),
-	new THREE.QuadraticBezierCurve3(
-		new THREE.Vector3( 0, -7.5, 0 ),
-		new THREE.Vector3( 2.5, -7.5, 0 ),
-		new THREE.Vector3( 2.5, -7.5, -2.5 )
-	),
-	new THREE.LineCurve3(
-		new THREE.Vector3( 2.5, -7.5, -2.5 ),
-		new THREE.Vector3( 2.5, -7.5, -12.5 )
-	),
-	new THREE.QuadraticBezierCurve3(
-		new THREE.Vector3( 2.5, -7.5, -12.5 ),
-		new THREE.Vector3( 2.5, -7.5, -15 ),
-		new THREE.Vector3( 0, -7.5, -15 )
-	),
-	new THREE.LineCurve3(
-		new THREE.Vector3( 0, -7.5, -15 ),
-		new THREE.Vector3( -20, -7.5, -15 )
-	),
-];
-
 function pointPathToStraightSegmentCurvePath(arr) {
 	let curvePath = new THREE.CurvePath();
 	for (let i = 0; i < (arr.length - 1); i++) {
@@ -141,18 +81,16 @@ function pointPathToStraightSegmentCurvePath(arr) {
 			)
 		)
 	}
-	console.log(curvePath);
 	return curvePath
+}
+
+function pointPathToLineAndElbowPath(arr) {
+
 }
 
 const myPipeRun = new PipeRun()
 myPipeRun.generatePath()
 const mycurvepath = pointPathToStraightSegmentCurvePath(myPipeRun.path);
-
-// const mycurvepath = new THREE.CurvePath();
-// for (let i = 0; i < testpipes.length; i++) {
-// 	mycurvepath.curves.push(testpipes[i])
-// }
 
 const materialargs = {
 	color: 0xffffff,
@@ -162,7 +100,7 @@ const materialargs = {
 };
 
 const material = new THREE.MeshPhongMaterial( materialargs );
-const geometry = new THREE.TubeGeometry(mycurvepath, 512, 0.5, 8, false);
+const geometry = new THREE.TubeGeometry(mycurvepath, 2048, 0.5, 8, false);
 const mesh = new THREE.Mesh( geometry, material );
 scene.add(mesh);
 
