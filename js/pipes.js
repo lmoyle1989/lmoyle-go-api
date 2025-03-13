8b8cdb3cb638b3016d0575ac42b9fa54ba310395
Lucas Moyle <lmoyle@gmail.com>
2025-03-13 15:19:26 -0700

    add quadratic curve elbows to path

--- js/pipes.js
+++ js/pipes.js
@@ -9,49 +9,91 @@ const dirs = [
 	{x: 0, y: 0, z: -1}
 ]
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 class PipeRun {
-	constructor() {
-		// this.materialargs = materialargs;
-		this.maxlength = 100;
-		this.startpos = {x: 0, y: 0, z: 0};
-		this.visited = new Set();
-		this.path = [];
+	constructor(materialargs, maxlength, startpos) {
+		this.materialargs = materialargs;
+		this.maxlength = maxlength;
+		this.startpos = startpos;
+
+		this.generatePath();
+		this.generateCurve();
+		this.generateMesh();
 	}
 
 	generatePath() {
-		let cur = this.startpos;
-		let i;
+		this.visited = new Set();
+		this.path = [];
+		this.elbows = [];
+		this.curvePath = new THREE.CurvePath();
+		let curpos = this.startpos;
+		let step = 0;
 		let nextdir;
 		let nextpos;
+		let lastdir;
 		const l = 5;
-		while (this.maxlength >= 0) {
-			this.visited.add(JSON.stringify(cur));
-			this.path.push(cur);
+		while (step <= this.maxlength) {
+			lastdir = nextdir;
 			do {
-				i = getRandomInt(dirs.length)
-				nextdir = dirs[i];
-				nextpos = {x: cur.x + (nextdir.x * l), y: cur.y + (nextdir.y * l), z: cur.z + (nextdir.z * l)}; // dont repeat this
+				nextdir = dirs[getRandomInt(dirs.length)];
+				nextpos = {
+					x: curpos.x + (nextdir.x * l),
+					y: curpos.y + (nextdir.y * l),
+					z: curpos.z + (nextdir.z * l)
+				};
 			} while ( this.visited.has(JSON.stringify(nextpos)) );
+			if (!(lastdir === nextdir)) {
+				this.elbows.push(step * l)
+			}
+			this.visited.add(JSON.stringify(curpos));
+			this.path.push(curpos);
 			if (this.maxlength != 0) {
 				for (let j = 1; j < l; j++) {
-					this.path.push({x: cur.x + (nextdir.x * j), y: cur.y + (nextdir.y * j), z: cur.z + (nextdir.z * j)});
+					this.path.push({
+						x: curpos.x + (nextdir.x * j),
+						y: curpos.y + (nextdir.y * j),
+						z: curpos.z + (nextdir.z * j)
+					});
 				}
 			}
-			cur = nextpos;
-			this.maxlength -= 1;
+			curpos = nextpos;
+			step += 1;
 		}
+	}
+
+	generateCurve() {
+		let j = 1
+		let lineStart = this.path[0]
+		for (let i = 0; i < this.path.length; i++) {
+			if (i == this.elbows[j]) {
+				this.curvePath.curves.push(
+					new THREE.LineCurve3(
+						new THREE.Vector3(lineStart.x, lineStart.y, lineStart.z),
+						new THREE.Vector3(this.path[i - 1].x, this.path[i - 1].y, this.path[i - 1].z)
+					)
+				)
+				if (j < this.elbows.length - 1) {
+					this.curvePath.curves.push(
+						new THREE.QuadraticBezierCurve3(
+							new THREE.Vector3(this.path[i - 1].x, this.path[i - 1].y, this.path[i - 1].z),
+							new THREE.Vector3(this.path[i].x, this.path[i].y, this.path[i].z),
+							new THREE.Vector3(this.path[i + 1].x, this.path[i + 1].y, this.path[i + 1].z)
+						)
+					)
+				}
+				lineStart = this.path[i + 1];
+				j += 1;
+			}
+		}
+	}
+
+	generateMesh() {
+		this.material = new THREE.MeshPhongMaterial(this.materialargs);
+		this.geometry = new THREE.TubeGeometry(this.curvePath, 16 * this.maxlength, 0.5, 8, false);
+		this.mesh = new THREE.Mesh( this.geometry, this.material );
 	}
 }
 
@@ -71,41 +113,37 @@ light.position.set( 100, 100, 100 );
 scene.add( light );
 scene.add( new THREE.AmbientLight( 0x777777 ) );
 
-function pointPathToStraightSegmentCurvePath(arr) {
-	let curvePath = new THREE.CurvePath();
-	for (let i = 0; i < (arr.length - 1); i++) {
-		curvePath.curves.push(
-			new THREE.LineCurve3(
-				new THREE.Vector3(arr[i].x, arr[i].y, arr[i].z),
-				new THREE.Vector3(arr[i + 1].x, arr[i + 1].y, arr[i + 1].z)
-			)
-		)
-	}
-	return curvePath
-}
-
-function pointPathToLineAndElbowPath(arr) {
-
-}
-
-const myPipeRun = new PipeRun()
-myPipeRun.generatePath()
-const mycurvepath = pointPathToStraightSegmentCurvePath(myPipeRun.path);
-
 const materialargs = {
-	color: 0xffffff,
+	color: 0xff0000,
 	specular: 0x050505,
 	shininess: 500,
 	emissive: 0x000000
 };
 
-const material = new THREE.MeshPhongMaterial( materialargs );
-const geometry = new THREE.TubeGeometry(mycurvepath, 2048, 0.5, 8, false);
-const mesh = new THREE.Mesh( geometry, material );
-scene.add(mesh);
+const materialargs2 = {
+	color: 0x0000ff,
+	specular: 0x050505,
+	shininess: 500,
+	emissive: 0x000000
+};
+
+const materialargs3 = {
+	color: 0x00ff00,
+	specular: 0x050505,
+	shininess: 500,
+	emissive: 0x000000
+};
+
+const myPipeRun = new PipeRun(materialargs, 100, {x: -5, y: -5, z: -5});
+const myPipeRun2 = new PipeRun(materialargs2, 100, {x: 5, y: 5, z: 5});
+const myPipeRun3 = new PipeRun(materialargs3, 100, {x: 0, y: 0, z: 0});
+
+scene.add(myPipeRun.mesh);
+scene.add(myPipeRun2.mesh);
+scene.add(myPipeRun3.mesh);
 
 let renderedIndices = 0;
-const totalIndices = geometry.index.count; // ~ tubeSegments * radial segments * 6
+const totalIndices = myPipeRun.geometry.index.count; // ~ tubeSegments * radial segments * 6
 const speed = 48; // 48 is the number of indices that makes up a single tube segment with radialsegments = 8
 
 function animate() {
@@ -114,7 +152,9 @@ function animate() {
 		renderedIndices += speed;
 	}
 
-	geometry.setDrawRange(0, Math.floor(renderedIndices));
+	myPipeRun.geometry.setDrawRange(0, Math.floor(renderedIndices));
+	myPipeRun2.geometry.setDrawRange(0, Math.floor(renderedIndices));
+	myPipeRun3.geometry.setDrawRange(0, Math.floor(renderedIndices));
 
 	renderer.render( scene, camera );
 }
