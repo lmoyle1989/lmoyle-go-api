dbd7a162290386d24bd66c9eb687b8954c9fb12f
Lucas Moyle <lmoyle@gmail.com>
2025-03-16 12:47:05 -0700

    add end sphere

--- js/pipes.js
+++ js/pipes.js
@@ -1,7 +1,13 @@
 //	TODOs:
 //	1. Make the pipe runs animate in sequence, potentially, instead of all at the same time
-//	2. Hide the unrendered pipe ends with another sphere
+//	2. Maybe keep rerendering the pipe end as it grows?
 //	3. camera controls?
+//	4. Number of tube segments needs to be a function of final length, not intial val - short tubes that deadend go very slow
+// 	5. mess around with different material values
+//	6. add more comments
+//	7. the end conditions and end position are kinda jank and don't lie on the grid, and sometimes just skips the last segment
+//	8. cleanup code: add all the scene setup stuff into its own function
+//	9. add interface to control number of pipes? or other global params?
 
 import * as THREE from 'three';
 
@@ -56,55 +62,55 @@ class PipeRun {
 	generatePath() {
 		this.path = [];
 		this.elbows = [];
-		let curpos = this.startPos;
+		let curPos = this.startPos;
 		let step = 0;
-		let nextdir;
-		let nextpos;
-		let lastdir;
-		let finished = false;
+		let nextDir;
+		let nextPos;
+		let lastDir;
+		let deadEnd = false;
 		const l = this.gridSize;
 		const straightness = 2;
-		while ( step <= this.maxLength ) {
+		while (step <= this.maxLength) {
 			let dirs = directions.slice();
-			if (lastdir) {
-				for ( let i = 0; i < straightness; i++ ) {
-					dirs.push(lastdir);
+			if (lastDir) {
+				for (let i = 0; i < straightness; i++) {
+					dirs.push(lastDir);
 				}
 			}
 			shuffleArray(dirs)
 			do {
 				if (dirs.length) {
-					nextdir = dirs.pop();
+					nextDir = dirs.pop();
 				}
 				else {
-					finished = true;
+					deadEnd = true;
 					break;
 				}
-				nextpos = {
-					x: curpos.x + (nextdir.x * l),
-					y: curpos.y + (nextdir.y * l),
-					z: curpos.z + (nextdir.z * l)
+				nextPos = {
+					x: curPos.x + (nextDir.x * l),
+					y: curPos.y + (nextDir.y * l),
+					z: curPos.z + (nextDir.z * l)
 				};
-			} while (this.visited.has(JSON.stringify(nextpos)));
-			this.visited.add(JSON.stringify(curpos));
-			this.path.push(curpos);
-			if (finished) {
+			} while (this.visited.has(JSON.stringify(nextPos)));
+			this.visited.add(JSON.stringify(curPos));
+			this.path.push(curPos);
+			if (deadEnd) {
 				break;
 			}
-			if (!(lastdir === nextdir)) {
+			if (!(lastDir === nextDir)) {
 				this.elbows.push(step * l)
 			}
-			if ( this.step != this.maxLength ) {
-				for ( let j = 1; j < l; j++ ) {
+			if (this.step != this.maxLength) {
+				for (let j = 1; j < l; j++) {
 					this.path.push({
-						x: curpos.x + (nextdir.x * j),
-						y: curpos.y + (nextdir.y * j),
-						z: curpos.z + (nextdir.z * j)
+						x: curPos.x + (nextDir.x * j),
+						y: curPos.y + (nextDir.y * j),
+						z: curPos.z + (nextDir.z * j)
 					});
 				}
 			}
-			lastdir = nextdir;
-			curpos = nextpos;
+			lastDir = nextDir;
+			curPos = nextPos;
 			step += 1;
 		}
 	}
@@ -113,15 +119,15 @@ class PipeRun {
 		this.curvePath = new THREE.CurvePath();
 		let j = 1
 		let lineStart = this.path[0]
-		for ( let i = 0; i < this.path.length; i++ ) {
-			if ( i == this.elbows[j] ) {
+		for (let i = 0; i < this.path.length; i++) {
+			if (i == this.elbows[j]) {
 				this.curvePath.curves.push(
 					new THREE.LineCurve3(
 						new THREE.Vector3(lineStart.x, lineStart.y, lineStart.z),
 						new THREE.Vector3(this.path[i - 1].x, this.path[i - 1].y, this.path[i - 1].z)
 					)
 				);
-				if ( j < this.elbows.length - 1 ) {
+				if (j < this.elbows.length - 1) {
 					this.curvePath.curves.push(
 						new THREE.QuadraticBezierCurve3(
 							new THREE.Vector3(this.path[i - 1].x, this.path[i - 1].y, this.path[i - 1].z),
@@ -149,8 +155,15 @@ class PipeRun {
 		if (this.curvePath.curves.length > 1) {
 			this.pipeGeometry = new THREE.TubeGeometry(this.curvePath, 16 * this.maxLength, 0.5, 8, false);
 			this.pipeMesh = new THREE.Mesh(this.pipeGeometry, this.material);
-			this.totalIndices = this.pipeGeometry.index.count; // = tubeSegments * radial segments * 6
+			this.totalIndices = this.pipeGeometry.index.count;
 			this.group.add(this.pipeMesh);
+
+			let endPoint = this.curvePath.curves[this.curvePath.curves.length - 1].v2;
+			this.endSphereGeometry = new THREE.SphereGeometry(0.75, 32, 16);
+			this.endSphereMesh = new THREE.Mesh(this.endSphereGeometry, this.material);
+			this.endSphereMesh.position.set(endPoint.x, endPoint.y, endPoint.z);
+			this.endSphereMesh.visible = false;
+			this.group.add(this.endSphereMesh);
 		}
 	}
 
@@ -159,6 +172,7 @@ class PipeRun {
 		this.startSphereGeometry.dispose();
 		if (this.pipeGeometry) {
 			this.pipeGeometry.dispose();
+			this.endSphereGeometry.dispose();
 		}
 	}
 }
@@ -189,6 +203,7 @@ class PipeGroup {
 			};
 		} while (this.visited.has(JSON.stringify(startPos)))
 		this.visited.add(JSON.stringify(startPos));
+		
 		return startPos
 	}
 
@@ -221,12 +236,17 @@ scene.add(new THREE.AmbientLight(0x777777));
 
 let renderedPipeGroup;
 const speed = 48; // 48 is the number of indices that makes up a single tube segment with radialsegments = 8
+									// total = tubeSegments * radial segments * 6
 
 function animate() {
 	for ( let i = 0; i < renderedPipeGroup.pipes.length; i++) {
-		if (renderedPipeGroup.pipes[i].renderedIndices < renderedPipeGroup.pipes[i].totalIndices) {
-			renderedPipeGroup.pipes[i].renderedIndices += speed;
-			renderedPipeGroup.pipes[i].pipeGeometry.setDrawRange(0, Math.floor(renderedPipeGroup.pipes[i].renderedIndices));
+		let pipe = renderedPipeGroup.pipes[i];
+		if (pipe.renderedIndices < pipe.totalIndices) {
+			pipe.renderedIndices += speed;
+			pipe.pipeGeometry.setDrawRange(0, Math.floor(pipe.renderedIndices));
+			if (pipe.renderedIndices >= pipe.totalIndices) {
+				pipe.endSphereMesh.visible = true;
+			}
 		}
 	}
 
@@ -254,7 +274,7 @@ function resetAnimation() {
 		scene.remove(renderedPipeGroup.group);
 		renderedPipeGroup.cleanup();
 	}
-	renderedPipeGroup = new PipeGroup(6);
+	renderedPipeGroup = new PipeGroup(10);
 	scene.add(renderedPipeGroup.group);
 }
 
